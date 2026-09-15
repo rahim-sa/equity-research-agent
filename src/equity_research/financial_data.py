a7f86f81ec25741e49c82902342efab6d517858e
rahimsadighi <sadighi.rahim@gmail.com>
2026-09-15 19:41:42 +0330

    feat: add 1-year price performance calculation

--- src/equity_research/financial_data.py
+++ src/equity_research/financial_data.py
@@ -4,12 +4,18 @@
 def get_financial_data(ticker):
     stock = yf.Ticker(ticker)
     info = stock.info
+    hist = stock.history(period="1y")
+
+    start_price = hist["Close"].iloc[0]
+    end_price = hist["Close"].iloc[-1]
+    perf_1y = round(((end_price - start_price) / start_price) * 100, 1)
 
     return f"""
 Name: {info["longName"]}
 Sector: {info["sector"]}
 Industry: {info["industry"]}
 Price: {info["currentPrice"]}
+1-Year Performance: {perf_1y}%
 Market Cap: {info["marketCap"]}
 Trailing P/E: {info["trailingPE"]}
 Forward P/E: {info["forwardPE"]}
