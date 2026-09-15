292961e10b96be291914c4e638d16a356c5a3adb
rahimsadighi <sadighi.rahim@gmail.com>
2026-09-15 19:19:12 +0330

    refactor: wrap financial data lookup in a function

--- src/equity_research/financial_data.py
+++ src/equity_research/financial_data.py
@@ -1,10 +1,15 @@
 ﻿import yfinance as yf
 
-stock = yf.Ticker("AAPL")
-info = stock.info
 
-print("Name:", info["longName"])
-print("Sector:", info["sector"])
-print("Price:", info["currentPrice"])
-print("Market Cap:", info["marketCap"])
-print("P/E:", info["trailingPE"])
+def get_financial_data(ticker):
+    stock = yf.Ticker(ticker)
+    info = stock.info
+
+    print("Name:", info["longName"])
+    print("Sector:", info["sector"])
+    print("Price:", info["currentPrice"])
+    print("Market Cap:", info["marketCap"])
+    print("P/E:", info["trailingPE"])
+
+
+get_financial_data("AAPL")
